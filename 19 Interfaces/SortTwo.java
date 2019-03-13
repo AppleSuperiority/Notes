652e9a2411a49892829db5732d0cc3b839a1d352
AppleSuperiority <titanswrath288@gmail.com>
2019-03-13 14:10:21 -0500

    i made it print out bad guys

--- 19 Interfaces/SortTwo.java
+++ 19 Interfaces/SortTwo.java
@@ -8,26 +8,46 @@ import java.util.Arrays;
 
 public class SortTwo
 {
-	public static void main ( String[] args )
-	{
-		Comparable[] list =  {3,8,7,6,5,4,9};
-		
-		Arrays.sort(list); //uses the compareTo() method of each object
-		
-		for(Comparable num : list)
-		{
-			out.println(num);
-		}
-
-		out.println("\n\n");
-
-		list = new String[]{"a","x","e","w","q"};
-		
-		Arrays.sort(list);
-		
-		for(Comparable let : list)
-		{
-			out.println(let);
-		}
+ public static void main ( String[] args )
+ {
+  Comparable[] list =  {3,8,7,6,5,4,9};
+  
+  Arrays.sort(list); //uses the compareTo() method of each object
+  
+  for(Comparable num : list)
+  {
+   out.println(num);
   }
+
+  out.println("\n\n");
+
+  list = new String[]{"a","x","e","w","q"};
+  
+  Arrays.sort(list);
+  
+  for(Comparable let : list)
+  {
+   out.println(let);
+  }
+  System.out.println("\n\n\n");
+  
+ 
+ Word a = new Word("saddam hussein");
+ Word b = new Word("Mao");
+ Word c = new Word("osama bin laden");
+ Word d = new Word("fidel castro");
+ Word e = new Word("hitler");
+ Word f = new Word("senor stalin");
+ Word g = new Word("mussolini");
+ Word h = new Word("ghengis khan");
+ Word i = new Word("kim jong un");
+ Word j = new Word("kim jong il");
+ list = new Word[]{a,b,c,d,e,f,g,h,i,j};
+ Arrays.sort(list);
+ 
+  for(Comparable let : list)
+  {
+   out.println(let);
+  }
+}
 }
